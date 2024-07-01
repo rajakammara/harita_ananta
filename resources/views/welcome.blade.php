873179744d0daac0ca6e2c33ec13ae1861ec75b5
Raja <kraja200709@gmail.com>
2024-07-01 08:19:37 +0530

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -423,11 +423,7 @@
         @endif
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8 text-lg">
-
-            Harita Ananta
-
-
-
+            Harita Anantapuramu
         </div>
     </div>
 </body>
